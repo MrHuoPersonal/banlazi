6c8fb5a8f0752a821ce97aa8c8617839b5d6a1f5
hdx <1490307216@qq.com>
2021-09-30 17:33:32 +0800

    banlazi项目初始化

---
+++ src/main/java/cn/banlazi/BanlaziApplication.java
@@ -0,0 +1,13 @@
+package cn.banlazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanlaziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanlaziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/banlazi/BanlaziApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.banlazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanlaziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
